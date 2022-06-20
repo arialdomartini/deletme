fc3cd6cb108672ce855ca4f6cea67d95812b66d4
Arialdo Martini <arialdomartini@kpmg.com>
2022-06-20 11:30:25 +0200

    P-Rider reads .runsettings; VS must be configured to autodiscover it

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(
+                "bar",
+                Environment.GetEnvironmentVariable("foo"));
+        }
+    }
+}
